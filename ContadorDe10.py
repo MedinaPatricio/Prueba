f124d360d2152cc7af9bf72714a1bc28f14b1687
unknown <patricimedinaj@gmail.com>
2022-08-07 22:56:36 -0300

    subo programa

---
+++ ContadorDe10.py
@@ -0,0 +1,32 @@
+
+
+import time
+cantidad = 10
+
+
+# ~ while cantidad != 0:
+	# ~ print (cantidad)
+	# ~ if cantidad == 5:
+		# ~ print ("Estamos a la mitad")
+	# ~ elif cantidad == 3:
+		# ~ print ("Prendan motores")
+	# ~ elif cantidad == 1:
+		# ~ print ("Despeguen")
+	# ~ cantidad -= 1
+
+
+while True:
+	print (cantidad)
+	if cantidad == 5:
+		print ("Estamos a la mitad")
+	elif cantidad == 3:
+		print ("Prendan motores")
+	elif cantidad == 1:
+		print ("Despeguen")
+	cantidad -= 1
+	if cantidad == 0:
+		break
+	time.sleep(1)
+# ~ lista = [1,2,3,4,5,6,7,8,9,10]
+# ~ nueva = list(reversed(lista))
+# ~ print(nueva)
